ecddf7e88505e497b58915ba9b8d2da75c805be2
ivan <ivanmarmilich@gmail.com>
2022-05-22 02:45:27 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio6-AdivinarNumero/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Ingrese numero");
+int numeroIngresado = int.Parse(Console.ReadLine());
+
+int numeroAleatorio = new Random().Next(0, 20);
+
+int contadorIntentos = 0;
+do
+{
+    if (numeroIngresado > numeroAleatorio)
+    {
+        Console.WriteLine("Numero ingresado mayor, intente de nuevo");
+        contadorIntentos++;
+
+        Console.WriteLine("Ingrese numero");
+        numeroIngresado = int.Parse(Console.ReadLine());
+        
+    } else
+    {
+        Console.WriteLine("Numero ingresador menor, intente de nuevo");
+        contadorIntentos++;
+
+        Console.WriteLine("Ingrese numero");
+        numeroIngresado = int.Parse(Console.ReadLine());
+        
+    }
+} while (numeroIngresado != numeroAleatorio);
+
+Console.WriteLine($"Felicitaciones, has adivinado el " +
+    $"número secreto que era {numeroAleatorio} en una cantidad de " +
+    $"intentos = {contadorIntentos}");
